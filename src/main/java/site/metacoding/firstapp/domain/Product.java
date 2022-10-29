f52a8d556640895086409bb764aaa3507441eea2
mysjw1028 <108706248+mysjw1028@users.noreply.github.com>
2022-10-29 11:48:20 +0900

    xml 수정

--- src/main/java/site/metacoding/firstapp/domain/Product.java
+++ src/main/java/site/metacoding/firstapp/domain/Product.java
@@ -6,29 +6,32 @@ import lombok.Getter;
 
 @Getter
 public class Product {
-    private Integer productId;
-    private String productName;
-    private Integer productPrice;
-    private Integer productQty;
-    private Timestamp createdAt;
+	private Integer productId;
+	private String productName;
+	private Integer productPrice;
+	private Integer productQty;
+	private Timestamp createdAt;
 
-    public void setProductId(Integer productId) {
-        this.productId = productId;
-    }
+	// 엔티티가 아닌 필드
+	private Integer no;
 
-    private Product() {
-    } // MyBatis에게 필요한 것
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
 
-    public Product(String productName, Integer productPrice, Integer productQty) {
-        this.productName = productName;
-        this.productPrice = productPrice;
-        this.productQty = productQty;
-    }
+	private Product() {
+	} // MyBatis에게 필요한 것
 
-    public void update(Product product) {
-        this.productName = product.getProductName();
-        this.productPrice = product.getProductPrice();
-        this.productQty = product.getProductQty();
-    }
+	public Product(String productName, Integer productPrice, Integer productQty) {
+		this.productName = productName;
+		this.productPrice = productPrice;
+		this.productQty = productQty;
+	}
+
+	public void update(Product product) {
+		this.productName = product.getProductName();
+		this.productPrice = product.getProductPrice();
+		this.productQty = product.getProductQty();
+	}
 
 }
--- src/main/webapp/WEB-INF/views/product/product.jsp
+++ src/main/webapp/WEB-INF/views/product/product.jsp
@@ -10,28 +10,30 @@
 <title>숙제</title>
 </head>
 <body>
-<div id ="cotainer'">
-	<h2> 상품전체보기</h2>
-	<table class="table table-product" id="datatable"  border="2" >
-		<thead>
-			<tr>
-				<th>이름</th>
-				<th>가격</th>
-				<th>재고</th>
-				<th>시간</th>
-			</tr>
-		</thead>
-		<tbody>
-			<c:forEach var="product" items="${product}">
+	<div id="cotainer'">
+		<h2>상품전체보기</h2>
+		<table class="table table-product" id="datatable" border="2">
+			<thead>
 				<tr>
-					<td>${product.productName}</td>
-					<td>${product.productPrice}</td>
-					<td>${product.productQty}</td>
-					<td>${product.createdAt}</td>
+					<th>번호</th>
+					<th>이름</th>
+					<th>가격</th>
+					<th>재고</th>
+					<th>시간</th>
 				</tr>
-			</c:forEach>
-		</tbody>
-	</table>
+			</thead>
+			<tbody>
+				<c:forEach var="product" items="${product}">
+					<tr>
+						<td>${product.no}</td>
+						<td>${product.productName}</td>
+						<td>${product.productPrice}</td>
+						<td>${product.productQty}</td>
+						<td>${product.createdAt}</td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
 	</div>
 </body>
 </html>
